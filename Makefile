dc2fe89704bb887f84918ee8890ef6d95096f437
Juan Pablo García Garland <jpgarcia@fing.edu.uy>
2019-10-18 22:22:05 -0300

    add module example

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-test1:
+test1: testsdl_1.ml
 	ocamlfind ocamlopt -o testsdl_1 -linkpkg   -package sdl,sdl.sdlimage,sdl.sdlmixer,sdl.sdlttf   testsdl_1.ml
+
+test2: tst2.ml
+	ocamlfind ocamlopt -o test2 -linkpkg   -package sdl,sdl.sdlimage,sdl.sdlmixer,sdl.sdlttf tst2.ml
+
+all: test1, test2
---
+++ tst2.ml
@@ -0,0 +1,57 @@
+let image_filename = "./resources/img.jpg"
+let font_filename  = "./resources/font.ttf"
+let music_filename = "./resources/music.mp3"
+
+let height      = 32
+let width       = 64
+let pixel_size  = 10
+
+module type FRAMEBUFF =
+  sig
+    type framebuff
+    type screen = Sdlvideo.surface
+    val init : int -> int -> int -> framebuff
+    val set  : framebuff -> int -> int -> unit
+    val draw : framebuff -> unit
+    val get_surface : framebuff -> screen
+  end;;
+
+
+
+let run () =
+  let a = Array.make 10 1 in
+  for i = 0 to 9 do
+      Printf.printf "%i\n" a.(i)
+  done;
+  let x= 3 in
+  for i = 0 to 9 do
+    Printf.printf "%i\n" a.(i)
+  done;
+  let screen = Sdlvideo.set_video_mode 400 400 [`DOUBLEBUF] in
+  let image = Sdlloader.load_image image_filename in
+  let font = Sdlttf.open_font font_filename 24 in
+  let text = Sdlttf.render_text_blended font "Enjoy!" ~fg:Sdlvideo.white in
+  let music = Sdlmixer.load_music music_filename in
+  let position_of_image = Sdlvideo.rect 0 0 300 300 in
+  let position_of_text = Sdlvideo.rect 300 0 300 300 in
+  Sdlvideo.blit_surface ~dst_rect:position_of_image ~src:image ~dst:screen ();
+  Sdlvideo.blit_surface ~dst_rect:position_of_text ~src:text ~dst:screen ();
+  Sdlvideo.flip screen;
+  Sdlmixer.fadein_music music 1.0;
+  Sdltimer.delay 1000; (* fade in *)
+  Sdltimer.delay 6000; (* play *)
+  Sdlmixer.fadeout_music 2.0;
+  Sdltimer.delay 2000; (* fade out *)
+  Sdlmixer.halt_music ();
+  Sdlmixer.free_music music;;
+
+let main () =
+    Sdl.init [`VIDEO; `AUDIO];
+    at_exit Sdl.quit;
+    Sdlttf.init ();
+    at_exit Sdlttf.quit;
+    Sdlmixer.open_audio ();
+    at_exit Sdlmixer.close_audio;
+    run ()
+
+let _ = main ()
