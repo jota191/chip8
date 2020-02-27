bfe8d4587887db0e74244459e57126308e3c346d
Juan García Garland <jpgarcia@fing.edu.uy>
2020-02-27 00:33:34 -0300

    upda machine

--- src/machine.ml
+++ src/machine.ml
@@ -1,17 +1,20 @@
-type address = (hex, hex, hex)
+type hex = int
 
+and address = Address of hex * hex * hex
+
+and register = Reg of hex
 
 (** Algebraic datatype, an abstract assembly *)
-type opcode =
-  (* FLOW *)
-  
-  | Call_RCA_1802 of address
+and opcode =
+  | SYS of address
   (** Calls a program at address, not used in most roms according to
      wiki so this case won't be implemented in the short term, 0NNN *)
-  | Return
+  | RET
   (** Returns from a subroutine, 00EE *)
-  | Goto of address
+  | JP of address
   (** unconditional jump, 1NNN *)
-  | Call of address
-  | Clear
+  | CALL of address
+  (** Calls a subroutine at address, 2NNN *)
+  | CLS
   (** Clear screen, 00E0 *)
+  | SE of register * hex * hex
